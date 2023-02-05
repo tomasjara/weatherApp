5b88c61aed2876ae80b043d58b22184605c8664c
tomas jara <tomasignaciojd@gmail.com>
2023-02-05 15:17:28 -0300

    testing forecast data

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { OpenWeather } from './components/OpenWeather'
+import { ForecastData } from './components/forecastData'
+// import { OpenWeather } from './components/OpenWeather'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <OpenWeather />
+    <ForecastData />
   </React.StrictMode>,
 )
